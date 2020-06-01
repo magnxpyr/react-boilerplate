725af6253bb0d0b986f045332d1114dc3c8074c3
Gina Chiriac <gina.chiriac@tremend.com>
2020-06-01 13:03:33 +0300

    fix dispatch action

--- src/pages/login/actions.ts
+++ src/pages/login/actions.ts
@@ -1,6 +1,4 @@
 import {FETCH_USER} from "./constants";
-import {call} from "@redux-saga/core/effects";
-import {sendRequest} from "redux-saga-requests";
 
 export const fetchUser = () => ({
     type: FETCH_USER,
@@ -8,7 +6,3 @@ export const fetchUser = () => ({
       url: 'http://localhost:3000/db.json',
     },
 });
-
-export function* fetchUsersSaga() {
-  yield call(sendRequest, fetchUser(), { dispatchRequestAction: false });
- }
